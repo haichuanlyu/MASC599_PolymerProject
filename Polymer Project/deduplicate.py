e7b9f6d5356cfd40f0c818efcb0db67555910d08
Haichuan Lyu <lhclhc0502@gmail.com>
2020-04-12 11:40:30 -0700

    deduplicate

--- Polymer Project/deduplicate.py
+++ Polymer Project/deduplicate.py
@@ -1,10 +1,17 @@
+import numpy as np
 import pandas as pd
 
-csv_path_and_name = 'polymer_project_dataset_Ver4.0(basic).csv'
-save_path = 'polymer_project_dataset_Ver4.0_deduplicate(basic).csv'
+basic_csv_path_and_name = 'polymer_project_dataset_Ver4.0(basic).csv'
+basic_save_path = 'polymer_project_dataset_Ver4.0_deduplicate(basic).csv'
+compound_csv_path_and_name = 'polymer_project_dataset_Ver4.0(compound).csv'
+compound_save_path = 'polymer_project_dataset_Ver4.0_deduplicate(compound).csv'
 
-csv_file = pd.read_csv(csv_path_and_name)
-csv_result = csv_file.iloc[0:0, :].copy
-for i in range(1276):
-    pass
-print(csv_result)
+
+def main(csv_path_and_name, save_path):
+    csv_file = pd.read_csv(csv_path_and_name)
+    res = csv_file.groupby('PROPERTY: SMILES').aggregate(np.mean)
+    res.to_csv(save_path)
+
+
+main(basic_csv_path_and_name, basic_save_path)
+main(compound_csv_path_and_name, compound_save_path)
